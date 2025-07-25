21f1bf3d800d44e0524bce31e780ecce54994c30
Hamza <hamzasepal@gmail.com>
2025-07-26 00:47:52 +0500

    resize cursors

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,25 @@ import { useLayoutEffect, useState } from "react";
 import "./App.css";
 import rough from "roughjs";
 import type { Drawable } from "roughjs/bin/core";
-import { findElementAtPosition } from "./utiles";
+import { adjustCordinates, findElementAtPosition, generateId } from "./utiles";
 const generator = rough.generator();
 
 export type Element = {
+  id: string;
   x1: number;
   y1: number;
   x2: number;
   y2: number;
+  type: string;
   roughElement: Drawable;
+  offsetX?: number; // used for selection to displace the element from the point where it was clicked
+  offsetY?: number; // used for selection to displace the element from the point where
 };
 
 function App() {
-  const [action, setAction] = useState("drawing"); // or 'selecting', etc.
-  const [elementType, setElementType] = useState("line"); // or 'line', etc.
+  const [action, setAction] = useState("none"); // drawing, selecting
+  const [tool, setTooltype] = useState("line"); // rectangle, line, selection
+  const [selectedElement, setSelectedElement] = useState<Element | null>(null);
   const [elements, setElements] = useState<Element[]>([]);
 
   const createElement = (
@@ -23,7 +28,8 @@ function App() {
     y1: number,
     x2: number,
     y2: number,
-    elementType: string
+    elementType: string,
+    id?: string
   ): Element => {
     let roughElement: Drawable;
     if (elementType === "rectangle") {
@@ -31,7 +37,30 @@ function App() {
     } else if (elementType === "line") {
       roughElement = generator.line(x1, y1, x2, y2);
     }
-    return { x1, y1, x2, y2, roughElement };
+    return {
+      id: !id ? generateId() : id,
+      x1,
+      y1,
+      x2,
+      y2,
+      roughElement,
+      type: elementType,
+    };
+  };
+
+  const updateElement = (
+    id: string,
+    x1: number,
+    y1: number,
+    x2: number,
+    y2: number,
+    elementType: string
+  ) => {
+    const updatedElement = createElement(x1, y1, x2, y2, elementType, id);
+    const updatedElements = [...elements];
+    const index = updatedElements.findIndex((el) => el.id === id);
+    updatedElements[index] = updatedElement;
+    setElements(updatedElements);
   };
 
   useLayoutEffect(() => {
@@ -44,48 +73,109 @@ function App() {
     elements.forEach((element) => {
       roughtCanvas.draw(element.roughElement);
     });
-  });
+  }, [elements]);
 
   const onMouseDown = (e: React.MouseEvent<HTMLCanvasElement>) => {
     const { clientX, clientY } = e;
-    if (elementType === "selection") {
+    if (tool === "selection") {
       // Handle selection logic here
       // check if clientX, and clientY are within the bounds of any existing element
       const selectedElement = findElementAtPosition(clientX, clientY, elements);
+      if (selectedElement) {
+        console.log("Element selected", selectedElement);
+        // use of offset is necessary to displace the element from the point where it was clicked
+        const offsetX = clientX - selectedElement.x1;
+        const offsetY = clientY - selectedElement.y1;
+        setSelectedElement({ ...selectedElement, offsetX, offsetY });
+        setAction("selecting");
+      }
     } else {
       setAction("drawing");
-      const element = createElement(
-        clientX,
-        clientY,
-        clientX,
-        clientY,
-        elementType
-      );
+      const element = createElement(clientX, clientY, clientX, clientY, tool);
       setElements((prev) => [...prev, element]);
       console.log("Mouse Down", e.clientX, e.clientY);
     }
   };
+
   const onMouseUp = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    if (action === "drawing") {
+      console.log("Mouse Up", e.clientX, e.clientY);
+      const index = elements.length - 1;
+      const lastElement = elements[index];
+      const { x1, y1, x2, y2 } = lastElement;
+      console.log(x1, y1, x2, y2);
+
+      const { x1: X, x2: Y, y1: X2, y2: Y2 } = adjustCordinates(lastElement);
+      console.log(X, X2, Y, Y2, "news");
+
+      updateElement(lastElement.id, X, X2, Y, Y2, lastElement.type);
+      console.log("updated!");
+    }
     setAction("none");
   };
 
   const onMouseMove = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    const { clientX, clientY } = e;
+    if (tool === "selection") {
+      const hoveredElement = findElementAtPosition(clientX, clientY, elements);
+      e.currentTarget.style.cursor = hoveredElement ? "move" : "default";
+    }
+    if (tool === "resize") {
+      console.log("am resizing");
+      const position = findElementAtPosition(
+        clientX,
+        clientY,
+        elements
+      )?.position;
+      let cursor;
+      switch (position) {
+        case "tl":
+        case "start":
+        case "br":
+        case "end":
+          cursor = "nwse-resize";
+          break;
+        case "tr":
+        case "bl":
+          cursor = "nesw-resize";
+          break;
+
+        default:
+          cursor = "default";
+          break;
+      }
+      e.currentTarget.style.cursor = cursor;
+    }
     if (action === "drawing") {
       const index = elements.length - 1;
       const lastElement = elements[index];
-      const updatedElement = createElement(
+      updateElement(
+        lastElement.id,
         lastElement.x1,
         lastElement.y1,
-        e.clientX,
-        e.clientY,
-        elementType
+        clientX,
+        clientY,
+        tool
       );
-      const updatedElements = [...elements];
-      updatedElements[index] = updatedElement;
-      console.log("Updated Elements", updatedElements);
-      setElements(updatedElements);
-
       console.log("Mouse Move", e.clientX, e.clientY);
+    } else if (action === "selecting" && selectedElement) {
+      console.log("dragging element");
+      const { x1, y1, x2, y2 } = selectedElement;
+      const width = x2 - x1;
+      const height = y2 - y1;
+      console.log("ID :-", selectedElement.id);
+      // off set needed to displace the element from the point where it was clicked
+      const { offsetX = 0, offsetY = 0 } = selectedElement;
+      const newX1 = clientX - offsetX;
+      const newY1 = clientY - offsetY;
+      updateElement(
+        selectedElement.id,
+        newX1,
+        newY1,
+        width + newX1,
+        height + newY1,
+        selectedElement.type
+      );
     }
   };
 
@@ -97,8 +187,8 @@ function App() {
           <input
             type="radio"
             value="line"
-            checked={elementType === "line"}
-            onChange={(e) => setElementType(e.target.value)}
+            checked={tool === "line"}
+            onChange={(e) => setTooltype(e.target.value)}
           />
           Line
         </label>
@@ -106,8 +196,8 @@ function App() {
           <input
             type="radio"
             value="rectangle"
-            checked={elementType === "rectangle"}
-            onChange={(e) => setElementType(e.target.value)}
+            checked={tool === "rectangle"}
+            onChange={(e) => setTooltype(e.target.value)}
           />
           Rectangle
         </label>
@@ -115,10 +205,19 @@ function App() {
           <input
             type="radio"
             value="selection"
-            checked={elementType === "selection"}
-            onChange={(e) => setElementType(e.target.value)}
+            checked={tool === "selection"}
+            onChange={(e) => setTooltype(e.target.value)}
           />
           Selection
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="resize"
+            checked={tool === "resize"}
+            onChange={(e) => setTooltype(e.target.value)}
+          />
+          Resize
         </label>
       </div>
       <canvas
--- src/utiles.ts
+++ src/utiles.ts
@@ -1,9 +1,103 @@
 import type { Element } from "./App";
 
-export function findElementAtPosition(x: number, y: number, elements: Element[]) {
-    return elements.find(element => isWithinElement(x, y, element));
+const distance = (a: { x: number, y: number }, b: { x: number, y: number }) => {
+    return Math.sqrt((b.x - a.x) ** 2 + (b.y - a.y) ** 2);
 }
 
-const isWithinElement = (x: number, y: number, element: Element) => {
+
+export function findElementAtPosition(x: number, y: number, elements: Element[]) {
+    const existingElement = elements.find(element => positionWithinElement(x, y, element) !== "outside")
+    if(existingElement){
+        return {...existingElement, position: positionWithinElement(x, y, existingElement)}
+    }
     
+}
+
+const positionWithinElement = (x: number, y: number, element: Element) : string  => {
+    const { x1, y1, x2, y2, type } = element;
+    if (type === "rectangle") {
+        // check if point is at top-left, bottom-right, top-right, bottom-left or inside the rectangle
+        const isTopLeft = isPointNear(x, y, x1, y1);
+        const isTopRight = isPointNear(x, y, x2, y1);
+        const isBottomRight = isPointNear(x, y, x2, y2);
+        const isBottomLeft = isPointNear(x, y, x1, y2);
+        if(isTopLeft){
+            return "tl"
+        }else if(isTopRight) {
+            return "tr"
+        } else if(isBottomRight) {
+            return "br"
+        }
+        else if(isBottomLeft) {
+            return "bl"
+        }
+        return x >= x1 && x <= x2 && y >= y1 && y <= y2 ? "inside" : "outside";
+
+    } else if (type === "line") {
+        // For a point to be within a line, the sum of two distance from the points to endpoints should be equal(approx, or thora sa greater) to distance btw endpoints
+        // a, b are endpoints, c is the clicked point
+        const a = { x: x1, y: y1 };
+        const b = { x: x2, y: y2 };
+        const c = { x, y };
+
+        const threshold = distance(a, b) - (distance(a, c) + distance(b, c)) 
+
+        const isOnStart = isPointNear(x, y, x1, y1);
+        const isOnEnd = isPointNear(x, y, x2, y2);
+
+        if(isOnStart) {
+            return "start";
+        }else if(isOnEnd) {
+            return "end";
+        }
+
+        return Math.abs(threshold) < 1  ? 'inside' : 'outside'; // Adjust the threshold as needed
+    }else {
+        return "outside"; // default case, should not happen
+    }
+
+}
+
+// random id generator
+export const generateId = () => {
+    return Math.random().toString(36).substr(2, 9);
+}
+interface AdjustedCoordinates {
+    x1: number;
+    y1: number;
+    x2: number;
+    y2: number;
+}
+export const adjustCordinates = (element : Element) : AdjustedCoordinates => {
+    const { x1, y1, x2, y2, type } = element;
+    // Adjust cordinates such that x1, y1 are always top-left and x2, y2 are always bottom-right
+    if (type === "rectangle") {
+        console.log('old ones ', x1, y1, x2, y2);
+        console.log('new ones ', {
+            x1: Math.min(x1, x2),
+            y1: Math.min(y1, y2),
+            x2: Math.max(x1, x2),
+            y2: Math.max(y1, y2)
+        });
+        return {
+            x1: Math.min(x1, x2),
+            y1: Math.min(y1, y2),
+            x2: Math.max(x1, x2),
+            y2: Math.max(y1, y2)
+        };
+    } else if (type === "line") {
+        // lesser are always x1, greater are always x2 (except for vertical lines, when the upper is x1,y1, lower is x2,y2)
+        const isVertical = x1 === x2;
+        if( (x1 < x2 ) || (isVertical && y1 < y2) ) {
+        return { x1, y1, x2, y2 };
+        }else {
+            return { x1: x2, y1: y2, x2: x1, y2: y1 };
+        }
+    }
+    return { x1, y1, x2, y2 }; // default case, should not happen
+}
+
+export const isPointNear = (x : number, y : number, x1 : number, y1 : number): boolean => {
+
+return Math.abs(x - x1) < 10 && Math.abs(y - y1) < 10;
 }
